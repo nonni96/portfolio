91e6cfb9b0590ba76eeccd758980040c65e87523
nonni <nonni96@gmail.com>
2022-01-11 20:40:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 shadow-lg">
+      <div className="font-mono grid grid-cols-1 gap-5 xl:grid-cols-2 p-10">
+        <div className="p-7 bg-gradient-to-r from-purple-500 to-pink-500 shadow-md rounded">
+          <h1 className="text-xl">Veðurstofan</h1>
+          <p className="text-sm">Sumarvinna</p>
+          <p className="">Gerð hugbúnaðar sem auðveldar uppsetningu á vefmyndavélum. Notað
+                var Python með Flask frameworkinu og Javascript til að búa til Shell
+                scriptur og möppur sem þurfti.<br/>
+                Einnig gervigreindar verkefni sem snérist um að greina ský út frá
+                myndum vefmyndavéla veðurstofunnar. Þjálfunargögn voru gerð með
+                notkun maska á myndum og var notast við U-net til að þjálfa gögnin.</p>
+        </div>
+        <div className="p-7 bg-gradient-to-r from-purple-500 to-pink-500 shadow-md rounded">
+          <h1 className="text-xl">Veðurstofan</h1>
+          <p className="">hallo</p>
+        </div>
+      </div>
     </div>
   );
 }
