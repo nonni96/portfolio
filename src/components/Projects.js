d2ca232e76ad5fa1bd9a0a09dcaa11753be49039
nonni <nonni96@gmail.com>
2022-02-23 22:26:42 +0000

    new text for site

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,13 +6,15 @@ function Projects(icelandic) {
         <h1 className="text-center sm:text-7xl text-5xl pt-4">{icelandic.icelandic ? language.projects.is.title : language.projects.en.title}</h1>
         <div className="grid grid-cols-1 gap-10 xl:grid-cols-2 p-20">
           <div className="p-7 bg-gradient-to-r from-blue-500 to-teal-500 shadow-md rounded transition ease-in-out delay-100 hover:scale-105">
-          <h1 className="text-xl">{icelandic.icelandic ? language.projects.is.joklavefsja.title : language.projects.en.joklavefsja.title}</h1>
-          <p className="">{icelandic.icelandic ? language.projects.is.joklavefsja.text : language.projects.en.joklavefsja.text}</p>
+            <a href="https://islenskirjoklar.is" target='_blank'>
+              <h1 className="text-xl">{icelandic.icelandic ? language.projects.is.joklavefsja.title : language.projects.en.joklavefsja.title}</h1>
+              <p className="">{icelandic.icelandic ? language.projects.is.joklavefsja.text : language.projects.en.joklavefsja.text}</p>
+            </a>
           </div>
           <div className="p-7 bg-gradient-to-r from-blue-500 to-teal-500 shadow-md rounded transition ease-in-out delay-100 hover:scale-105">
             <a href="https://topguard.is" target='_blank'>
-            <h1 className="text-xl">{icelandic.icelandic ? language.projects.is.topguard.title : language.projects.en.topguard.title}</h1>
-          <p className="">{icelandic.icelandic ? language.projects.is.topguard.text : language.projects.en.topguard.text}</p>
+              <h1 className="text-xl">{icelandic.icelandic ? language.projects.is.topguard.title : language.projects.en.topguard.title}</h1>
+              <p className="">{icelandic.icelandic ? language.projects.is.topguard.text : language.projects.en.topguard.text}</p>
             </a>
           </div>
         </div>
--- src/lang.js
+++ src/lang.js
@@ -3,27 +3,27 @@ const lang = {
         'en': {
             title: 'Work',
             'imo': {
-                title: 'Icelandic Meteorological Office',
-                time: 'Part-time work for master thesis',
-                text: 'Development of the Glacier Web Portal'
+                title: 'Icelandic Meteorological Office - Full Stack Developer',
+                time: '09/21 - 05/22',
+                text: 'Making of the glacier web portal, islenskirjoklar.is. The project was used partly in my master’s thesis.'
             },
             'imosummer': {
                 title: 'Icelandic Meteorological Office',
                 time: 'Summer job',
-                text: 'Making of a software for the setup of webcams'
+                text: 'Making of a software to simplify the setup of webcams. Python was used with the Flask framework and Javascript was used for the frontend. The software returned Shell scripts and files that made the setup of webcams easier. The second project was a machine learning project that looked to distinguish clouds from images of webcams. The dataset was made with masking images and U-net was used to train the data.'
             }
         },
         'is': {
             title: 'Starfsreynsla',
             'imo': {
-                title: 'Veðurstofa Íslands',
-                time: 'Hlutastarf fyrir meistaraverkefni',
-                text: 'Gerð Jöklavefsjáar'
+                title: 'Veðurstofa Íslands - Full Stack Developer',
+                time: '09/21 - 05/22',
+                text: 'Gerð jöklavefsjáar, islenskirjoklar.is. Verkefnið var notað að hluta til í mastersritgerð.'
             },
             'imosummer': {
                 title: 'Veðurstofa Íslands',
                 time: 'Sumarstarf',
-                text: 'Gerð hugbúnaðar sem auðveldar uppsetningu á vefmyndavélum'
+                text: 'Gerð hugbúnaðar sem auðveldar uppsetningu á vefmyndavélum. Notað var Python með Flask frameworkinu og Javascript í framenda til að búa til Shell scriptur og möppur sem þurfti til uppsetningar. Einnig gervigreindar verkefni sem snérist um að greina ský út frá myndum vefmyndavéla veðurstofunnar. Þjálfunargögn voru gerð með notkun maska á myndum og var notast við U-net til að þjálfa gögnin.'
             }
         }
     },
@@ -31,19 +31,19 @@ const lang = {
         'en': {
             title: 'Projects',
             'joklavefsja': {
-                title: 'Glacier Web Portal',
-                text: 'Development of the Glacier Web Portal'
+                title: 'islenskirjoklar.is',
+                text: 'Javascript was used for the frontend and the Leaflet library for the map. The backend API made with python where data was fetched from a PostgreSQL database. Layers and markers on the map were fetched from a GeoServer that returned WFS and WMS data.'
             },
             'topguard': {
                 title: 'topguard.is',
-                text: 'Website made for Topguard. Made with HTML/CSS, UI after owners proposal'
+                text: 'Website made for Topguard. The website was made with HTML and CSS, appearance of the website made after the owners direction.'
             }
         },
         'is': {
             title: 'Verkefni',
             'joklavefsja': {
-                title: 'Jöklavefsjá',
-                text: 'Gerð Jöklavefsjáar'
+                title: 'islenskirjoklar.is',
+                text: 'Notast var við Javascript í framenda með Leaflet fyrir kortasjá. Bakenda API var gerður með python þar sem gögn voru sótt úr PostgreSQL gagnagrunni. Gögn á korti voru sótt af GeoServer sem skilaði WFS og WMS gögnum.'
             },
             'topguard': {
                 title: 'topguard.is',
